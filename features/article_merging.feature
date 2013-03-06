c23f86ac520dde9a2087feffb9d7846d2de70418
Craig J <alan.jjj@gmail.com>
2013-03-06 08:29:50 -0600

    Submit 1 for grade

--- features/article_merging.feature
+++ features/article_merging.feature
@@ -13,5 +13,45 @@ Feature: Article Merging (new)
 
   @spec1
   Scenario: A non-admin cannot merge two articles
+    Given I am logged in with a 'Non-admin' account
+    And I edit article '7'
+    Then I will 'not see' 'Merge Articles'
+    And I will log out
+    Given I am logged in with an 'admin' account
+    And I edit article '7'
+    Then I will 'see' 'Merge Articles'
+
+  @spec2
+  Scenario: When articles are merged, the merged article should contain the text of both previous articles
+    Given I am logged in with an 'admin' account
+    And I edit article '5'
+    Then I will 'see' 'Hi everybody <em>!!!'
+    And I edit article '7'
+    Then I will 'see' 'Hey; I mean, Hi'
+    And I merge article '7' to article '5'
+    Then I will 'see' 'Hi everybody <em>!!!'
+    And I will 'see' 'Hey; I mean, Hi'
+
+  @spec3
+  Scenario: When articles are merged, the merged article should have one author
+    Given I am logged in with an 'admin' account
+    And I edit article '5'
+    And I merge article '7' to article '5'
+    Then Article '5' will have author 'testadmin'
+
+  @spec4
+  Scenario: Comments of the original articles need to all carry over and point to the new, merged article
+    Given I am logged in with an 'admin' account
+    And I edit article '5'
+    And I merge article '7' to article '5'
+    Then Article '5' will have '4' comments
+
+  @spec5
+  Scenario: The title of the new article should be the title from either one of the merged articles
+    Given I am logged in with an 'admin' account
+    And I edit article '5'
+    And I merge article '7' to article '5'
+    Then Article '5' will have title 'Admin says hi'
+
 
 
--- features/step_definitions/article_merging_steps.rb
+++ features/step_definitions/article_merging_steps.rb
@@ -45,9 +45,48 @@ Given /^I have added my test user accounts$/ do
 end
 
 Given /^I have added my test articles$/ do
-  pending # express the regexp above with the code you wish you had
+  #-- Burn articles 3 and 4
+  (1..2).each {FactoryGirl.create(:article, {:title => 'An article', :body => 'A body'})}
+  #-- Article 5
+  FactoryGirl.create(:article,
+    {:author => 'testadmin',
+     :user_id => 3,
+     :title => 'Admin says hi',
+     :body => '<h1>Hi everybody <em>!!!</em></h1><p>&nbsp;</p>'})
+  #-- Burn article 6
+  FactoryGirl.create(:article, {:title => 'An article', :body => 'A body'})
+  #-- Article 7
+  FactoryGirl.create(:article,
+    {:author => 'pub1',
+     :user_id => 2,
+     :title => 'Hi from publisher 1',
+     :body => 'Hey; I mean, Hi.</p>'})
+
+  #-- Add comments
+  article_5 = Article.find(5); article_7 = Article.find(7)
+  FactoryGirl.create(:comment, {:article => article_5, :body => "Comment 1 on '#{article_5.title}'"})
+  FactoryGirl.create(:comment, {:article => article_5, :body => "Comment 2 on '#{article_5.title}'"})
+  FactoryGirl.create(:comment, {:article => article_7, :body => "Comment 1 on '#{article_7.title}'"})
+  FactoryGirl.create(:comment, {:article => article_7, :body => "Comment 2 on '#{article_7.title}'"})
+
+  assert( ((article_5.comments.count == 2) and (article_7.comments.count == 2)), 
+    'Test article creating failed' )
 end
 
+Then /^Article '(\d+)' will have author '(.*)'$/ do |article_id, author|
+  assert(Article.find(article_id).author == author,
+    "Expected author, #{author}, not found")
+end
+
+Then /^Article '(\d+)' will have '(\d+)' comments$/ do |article_id, number_of_comments|
+  assert(Article.find(article_id).comments.count == number_of_comments.to_i,
+    "Expected number of comments, #{number_of_comments}, not found")
+end
+
+Then /^Article '(\d+)' will have title '(.*)'$/ do |article_id, title|
+  assert(Article.find(article_id).title == title,
+    "Expected title, #{title}, not found")
+end
 
 
 
